7c8968611db95674cb79e80c118979010cdf2f2f
18100576 <zpw2017@e-chinalife.com>
2019-10-14 17:33:01 +0800

    2019-10-14 726

--- leetCode/src/com/leetcode/recursion/LeetCode726.java
+++ leetCode/src/com/leetcode/recursion/LeetCode726.java
@@ -4,114 +4,65 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+/**
+ * K4(ON(SO3)2)2
+ */
 public class LeetCode726 {
     public static void main(String[] args) {
-        String formula;
-        Scanner scanner = new Scanner(System.in);
-        formula = scanner.next();
-        String a = formula.substring(2);
-        System.out.println(a);
+//        String formula;
+//        Scanner scanner = new Scanner(System.in);
+//        formula = scanner.next();
+//        String a = formula.substring(2);
+//        System.out.println(a);
         Solution726 solution726 = new Solution726();
-        Map<String,Integer> map = new HashMap();
-        System.out.println(solution726.clean(formula,map,1));
+        solution726.countOfAtoms("K4(ON(SO3)2)2");
     }
 }
 
 class Solution726 {
-    public String clean(String formula,Map<String,Integer> map,int x) {
-        /*
-        按照传进来的倍数将传进来的内容数字全都 x 倍
-         */
-        String afterClean = "";
-        int begin = 0;
-        int endBegin = 0;
-        int endEnd = 0;
+    static Map<String, Integer> map = new HashMap<>();
+
+    public String countOfAtoms(String formula) {
         int flag = 0;
-        String count = "";
-        int countNum = 0;
-
-        String formulaTemp = formula;
-        formula = "";
+        String toAdd = "";//需要切分添加的字符串
+        String countString = "";
+        int countNum;
         for (int i = 0; i < formula.length(); i++) {
-            char temp = formula.charAt(i);
-            if (temp != '(' ) {
-                begin = i;//标记第一组 （ ） 开始位置  包含（
-                break;
+            if (formula.charAt(i) != '(' && formula.charAt(i) != ')') {
+                toAdd += formula.charAt(i);
             }
-            afterClean += temp; //没有括号的部分
-        }
-        int i = 0 ;
-        while(formula.charAt(i) >='0' && formula.charAt(i) <='9'){
-            count += formula.charAt(i);
-            i++;
-        }
-
-
-
-
-//        String afterClean = "";
-//        int begin = 0;
-//        int endBegin = 0;
-//        int endEnd = 0;
-//        int flag = 0;
-//        String count = "";
-//        int countNum = 0;
-        for (i = 0; i < formula.length(); i++) {
-            char temp = formula.charAt(i);
-            if (temp != '(' ) {
-                begin = i;//标记第一组 （ ） 开始位置  包含（
-                break;
-            }
-            afterClean += temp; //没有括号的部分
-        }
-        /*
-        需要将没有括号的部分放入map中
-         */
-        if(!map.containsKey(afterClean)){
-            map.put(afterClean,1);
-        }
-        for(i = begin; i < formula.length();i++){
-            char temp = formula.charAt(i);
-            if (temp == '(' ) {
+            if (formula.charAt(i) == '(') {
+                if (flag == 0) {
+                    System.out.println(toAdd);
+                    toAdd = "";//第一次遇到括号，将游标字符置空，前面获得的游标值存入
+                }else{
+                    toAdd += formula.charAt(i);
+                }
                 flag++;
             }
-            if(temp == ')'){
+            if (formula.charAt(i) == ')') {
                 flag--;
-                if(flag == 0){
-                    endBegin = i; //标记一组 （ ） 结束位置  包含）
+                if (flag == 0) {
                     i++;
-                    while(formula.charAt(i) >='0' && formula.charAt(i) <='9'){
-                        count += formula.charAt(i);
+                    while (formula.charAt(i) >= '0' && formula.charAt(i) <= '9') {
+                        countString += formula.charAt(i);
                         i++;
+                        if(i >= formula.length()){
+                            break;
+                        }
                     }
-                    endEnd = i;
-                    countNum = Integer.parseInt(count);//括号后的倍数
-                    break;
+                    countNum = Integer.parseInt(countString);//括号后的倍数
+                    System.out.println(toAdd + "," + countNum);
+
+                }else{
+                    toAdd += formula.charAt(i);
                 }
             }
         }
-        //括号中的部分 去掉 前后括号 把 后面的倍数
-        clean(formula.substring(begin + 1,endBegin -1),map,countNum);
+        return "";
+    }
 
-        //已经截取一组 （） 后面的部分继续
-        clean(formula.substring(endEnd),map,1);
-        return afterClean;
+    public String dealWithFormula() {
+        return "";
     }
 }
-// 去掉所有括号
-//  K4(ON(SO3)2)2
-/**
- * K4(ON(SO3)2)2    1
- * ON(SO3)2         2
- * SO3              2
- * S2O6             1
- * ONS2O6           1
- * O2N2S4O12        1
- * K4O2N2S4O12      1
- * 合并
- * K4O14N2S4
- * 判断ch是数字，条件是     (ch>='0' && ch<='9')
- * 判断ch是字符，条件是     (ch>='A' && ch<='Z'  ||  ch>='a' && ch<='z')
- * K4N2((OH)2H2(SO3)2)2
- */
-
